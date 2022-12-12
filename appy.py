ffc47647bafe2757bfab6015acbf2b698a264297
svlch <117685611+svlch@users.noreply.github.com>
2022-12-11 22:41:56 -0600

    Update appy.py

--- appy.py
+++ appy.py
@@ -6,9 +6,9 @@ t.pencolor('purple')
 t.speed(0)
 
 for i in range(320):
-  t.circle(190-i,90)
+  t.circle(190-i, 90)
   t.left(90)
-  t.circle(190-i,90)
+  t.circle(190-i, 90)
   t.left(18)
   if i>190:
     t.pensize(3)
