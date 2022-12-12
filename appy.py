28730f2af1fd3e4d2d7f3fe2512402c5a0ce7710
svlch <117685611+svlch@users.noreply.github.com>
2022-12-11 22:32:34 -0600

    Update and rename README.md to appy.py

---
+++ appy.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+t=turtle()
+bgcolor('black')
+t.pencolor('purple')
+t.speed(0)
+
+for i in range(320):
+  t.circle(190-i,90)
+  t.left(90)
+  t.circle(190-i,90)
+  t.left(18)
+  if i>190:
+    t.pensize(3)
